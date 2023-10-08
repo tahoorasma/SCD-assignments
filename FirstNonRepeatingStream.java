d8b25c0ab6713e309795781a86cf865de63e9430
Tahoor Asma <102382100+tahoorasma@users.noreply.github.com>
2023-10-08 22:26:05 +0500

    Add files via upload

--- FirstNonRepeatingStream.java
+++ FirstNonRepeatingStream.java
@@ -1,8 +1,9 @@
 package com.mycompany.assignment2.Q2;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FirstNonRepeatingStream {
+    private ArrayList<Character> list = new ArrayList<Character>();
     int size;
     char [] array;
     FirstNonRepeatingStream(){
@@ -12,62 +13,40 @@ public class FirstNonRepeatingStream {
     void add(char c){
         array[size] = c;
         size++;
+        if (list.contains(c)){
+            list.remove(Character.valueOf(c));
+            return;
+        }
+        else
+        list.add(c);
     }
     void display(){
+        if (size>0){
         System.out.print("Stream: ['");
         for (int i = 0; i<size-1; i++){
         System.out.print(array[i]+"', '");}
-        System.out.println(array[size-1]+"']");
+        System.out.println(array[size-1]+"']");}
+        else
+        System.out.println("No data");
     }
     char getFirstNonRepeating(){
-        char []arr = new char[128];
-        int s = 0;
-        boolean flag;
-        for (int i = 0; i<size; i++){
-            flag = false;
-            for (int j = i+1; j<size; j++){
-                if (array[i]==array[j]){
-                    flag = true;
-                    arr[s] = array[i];
-                    s++;
-                    break;
-                }  
-            }
-            for (int k = 0; k < s; k++){
-                    if (array[i] == arr[k]){
-                        flag = true;
-                        break;
-                    }
-                }
-            if (!flag)
-            return array[i];
-        }
+        if (!list.isEmpty())
+        return list.get(0);
         return '-';
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         FirstNonRepeatingStream stream = new FirstNonRepeatingStream();
-        int size;
-        //System.out.print("Enter size of stream: ");
-        //size = sc.nextInt();
         char ch;
-        //for (int i = 0; i<size; i++){
         int i = 0;
+        System.out.println("Enter 0 to stop");
+        while (i<100){
         System.out.print("Enter char at "+i+": ");
         ch = sc.next().charAt(0);
+        if (ch == '0') break;
         stream.add(ch);
         i++;
-        System.out.print("Enter char at "+i+": ");
-        ch = sc.next().charAt(0);
-        stream.add(ch);
-        i++;
-        System.out.print("Enter char at "+i+": ");
-        ch = sc.next().charAt(0);
-        stream.add(ch);
-        i++;
-        System.out.print("Enter char at "+i+": ");
-        ch = sc.next().charAt(0);
-        stream.add(ch);
+        }
         stream.display();
         System.out.println("Non-Repeating: "+stream.getFirstNonRepeating());
     }
